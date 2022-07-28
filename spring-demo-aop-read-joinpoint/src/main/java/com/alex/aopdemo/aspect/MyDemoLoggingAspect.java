6ea7f3082611ec7c156631512233f9788a3ce147
Alexandru Vasile <alexandru.vasile19999@gmai.com>
2022-07-28 16:33:23 +0300

    More AOP

---
+++ spring-demo-aop-read-joinpoint/src/main/java/com/alex/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,41 @@
+package com.alex.aopdemo.aspect;
+
+import com.alex.aopdemo.Account;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+
+    @Before("com.alex.aopdemo.aspect.AopExpressions.forDaoPackageNoGetterSetter()")
+    public void beforeAddAccountAdvice(JoinPoint joinPoint) {
+        System.out.println("\n=======>>> Executing @Before advice on method");
+
+        // display method signature
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        System.out.println("Method: " + methodSignature);
+
+        // display method arguments
+
+        // get args
+        Object[] args = joinPoint.getArgs();
+
+        // loop through args
+        for(Object arg : args) {
+            System.out.println(arg);
+            if(arg instanceof Account) {
+                // downcast and print Account specific stuff
+                Account account = (Account) arg;
+                System.out.println("Account name: " + account.getName());
+                System.out.println("Account level: " + account.getLevel());
+            }
+        }
+    }
+}
